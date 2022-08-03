b89aa91de492e307d92696b8ddbbd5faaa5907de
martinlaursen97 <martin_laursen21@hotmail.com>
2022-08-04 01:50:52 +0200

    can't create entity with taken name

--- src/components/modals/EntityModal.js
+++ src/components/modals/EntityModal.js
@@ -20,6 +20,8 @@ export default function EntityModal({ open, title, onClose, entities, setEntitie
     const [hasUpdate , setHasUpdate ] = useState(false);
     const [hasDelete , setHasDelete ] = useState(false);
 
+    const [error, setError] = useState("");
+
     useEffect(() => {
         (async function() {
             try {
@@ -36,61 +38,73 @@ export default function EntityModal({ open, title, onClose, entities, setEntitie
 
     if (!open) return null;
 
+    const nameTaken = n => {
+        if (entities.length > 1) {
+            for (let i = 0; i < entities.length; i++) {
+                if (entities[i].name === n) {
+                    setError("Name already taken")
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (name.length > 0) {
-            if (modalMethod === "POST") {
-                let entity = {
-                    project: {
-                        id: window.localStorage.getItem("projectId")
-                    },
-                    name: name,
-                    hasCreate: hasCreate,
-                    hasReadAll: hasReadAll,
-                    hasRead: hasRead,
-                    hasUpdate: hasUpdate,
-                    hasDelete: hasDelete
-                }
+            if (!nameTaken(name)) {
+                if (modalMethod === "POST") {
+                    let entity = {
+                        project: {
+                            id: window.localStorage.getItem("projectId")
+                        },
+                        name: name,
+                        hasCreate: hasCreate,
+                        hasReadAll: hasReadAll,
+                        hasRead: hasRead,
+                        hasUpdate: hasUpdate,
+                        hasDelete: hasDelete
+                    }
 
-                await CRUD.create(entity, "entities")
-                    .then(res => {
-                        let data = res.data;
-                        entities.push(data);
-                        setEntities(entities);
-                    })
-                    .then(close);
-            } else {
+                    await CRUD.create(entity, "entities")
+                        .then(res => {
+                            let data = res.data;
+                            entities.push(data);
+                            setEntities(entities);
+                        })
+                        .then(close);
+                } else {
 
+                    entity.name = name;
+                    entity.hasCreate = hasCreate;
+                    entity.hasReadAll = hasReadAll;
+                    entity.hasRead = hasRead;
+                    entity.hasUpdate = hasUpdate;
+                    entity.hasDelete = hasDelete;
 
-
-                entity.name = name;
-                entity.hasCreate = hasCreate;
-                entity.hasReadAll = hasReadAll;
-                entity.hasRead = hasRead;
-                entity.hasUpdate = hasUpdate;
-                entity.hasDelete = hasDelete;
-
-                await CRUD.update(entity, "entities", entity.id)
-                    .then(async () => {
-                        // Update dependencies
-                        if (entities.length > 1) {
-                            for (let i = 0; i < entities.length; i++) {
-                                if (entities[i].relations.length > 0 && entities[i].relations !== null) {
-                                    for (let j = 0; j < entities[i].relations.length; j++) {
-                                        let relatedToDependency = entities[i].relations[j].relatedTo;
-                                        if (relatedToDependency === prevName) {
-                                            entities[i].relations[j].relatedTo = name;
-                                            await CRUD.update(entities[i], "entities", entities[i].id)
+                    await CRUD.update(entity, "entities", entity.id)
+                        .then(async () => {
+                            // Update dependencies
+                            if (entities.length > 1) {
+                                for (let i = 0; i < entities.length; i++) {
+                                    if (entities[i].relations.length > 0 && entities[i].relations !== null) {
+                                        for (let j = 0; j < entities[i].relations.length; j++) {
+                                            let relatedToDependency = entities[i].relations[j].relatedTo;
+                                            if (relatedToDependency === prevName) {
+                                                entities[i].relations[j].relatedTo = name;
+                                                await CRUD.update(entities[i], "entities", entities[i].id)
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
-                    })
-                    .then(close);
+                        })
+                        .then(close);
+                }
+                resetStates(true);
             }
-            resetStates(true);
         }
     }
 
@@ -106,6 +120,7 @@ export default function EntityModal({ open, title, onClose, entities, setEntitie
         setHasRead   (isPut ? entity.hasRead    : bool);
         setHasUpdate (isPut ? entity.hasUpdate  : bool);
         setHasDelete (isPut ? entity.hasDelete  : bool);
+        setError("");
     }
 
     return ReactDom.createPortal(
@@ -122,12 +137,14 @@ export default function EntityModal({ open, title, onClose, entities, setEntitie
                             type="text"
                             aria-label="Default"
                             aria-describedby="inputGroup-sizing-default"
-                            onChange={e => setName(e.target.value)}
+                            onChange={e => {
+                                setName(e.target.value);
+                                setError("");
+                            }}
                             value={name}
                             placeholder={"City..."}
                         />
                     </InputGroup>
-
 
                     <Form.Check onChange={(e) => setHasCreate(e.target.checked)}  checked={hasCreate }  name="create" label="Create" inline/>
                     <Form.Check onChange={(e) => setHasReadAll(e.target.checked)} checked={hasReadAll} name="readAll" label="ReadAll" inline/>
@@ -138,6 +155,11 @@ export default function EntityModal({ open, title, onClose, entities, setEntitie
                 </Modal.Body>
 
                 <Modal.Footer>
+                    {error !== "" ?
+                        <p className={"error"}>{error}</p>
+                        :
+                        <p/>
+                    }
                     <Button variant="secondary" onClick={close}>Close</Button>
                     <Button variant="primary" onClick={handleSubmit}>{isPut ? "Update" : "Save" } </Button>
                 </Modal.Footer>
