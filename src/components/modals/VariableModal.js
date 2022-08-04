f6223cf9b4939d5e437a2e157919d8000936a235
martinlaursen97 <martin_laursen21@hotmail.com>
2022-08-04 02:22:34 +0200

    check variable modal inputs

--- src/components/modals/VariableModal.js
+++ src/components/modals/VariableModal.js
@@ -10,10 +10,14 @@ export default function VariableModal({ open, title, onClose, entity, setEntity,
 
     let isPut = modalMethod === "PUT";
 
+    const [prevName, setPrevName] = useState("");
+
     const [name, setName] = useState("");
     const [dataType, setDataType] = useState("");
     const [columnName, setColumnName] = useState("");
     const [hasColumn, setHasColumn] = useState(false);
+
+    const [error, setError] = useState("");
 
     const close = () => {
         resetStates(true);
@@ -26,11 +30,14 @@ export default function VariableModal({ open, title, onClose, entity, setEntity,
         setDataType("");
         setColumnName("");
         setHasColumn(false);
+        setPrevName("");
+        setError("");
     }
 
     useEffect(() => {
         (async function() {
             try {
+                setPrevName(  isPut ? selectedVariable.name : "");
                 setName (     isPut ? selectedVariable.name : "");
                 setDataType(  isPut ? selectedVariable.dataType : "");
                 setColumnName(isPut ? selectedVariable.columnName : "");
@@ -54,10 +61,46 @@ export default function VariableModal({ open, title, onClose, entity, setEntity,
     
     if (!open) return null;
 
+    const invalidDataTypeInput = d => {
+        if (d === 0) {
+            setError("Empty input")
+            return true;
+        }
+
+        if (d.includes(" ")) {
+            setError("Illegal character")
+            return true;
+        }
+
+        return false;
+    }
+
+    const variableNameTaken = n => {
+        if (dataType.length === 0) {
+            setError("Input missing");
+            return true;
+        }
+
+        if (entity.variables.length > 0) {
+            for (let i = 0; i < entity.variables.length; i++) {
+                if (entity.variables[i].name === prevName) {
+                    return false;
+                }
+                if (entity.variables[i].name === n) {
+
+                    setError("Name already taken");
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (name.length > 0 && dataType.length > 0) {
+        if (!variableNameTaken(name) && !invalidDataTypeInput(dataType)) {
             if (isPut) {
                 selectedVariable.name = name;
                 selectedVariable.dataType = dataType;
@@ -147,6 +190,11 @@ export default function VariableModal({ open, title, onClose, entity, setEntity,
                     </Modal.Body>
 
                     <Modal.Footer>
+                        {error !== "" ?
+                            <p className={"error"}>{error}</p>
+                            :
+                            <p/>
+                        }
                         {
                             isPut ?
                                 <Button variant="danger" onClick={remove}>Delete</Button>
